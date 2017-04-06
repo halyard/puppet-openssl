c07db8f5455bc49db79ab2d9d6086d424b224250
Les Aker <me@lesaker.org>
2017-04-05 23:26:41 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 class openssl (
 ) {
-  package { 'openssl': } ->
-  exec { 'brew link --force openssl':
+  package { 'openssl': }
+  -> exec { 'brew link --force openssl':
     provider => 'shell',
     unless   => "test -f $(brew --prefix)/bin/openssl"
   }
